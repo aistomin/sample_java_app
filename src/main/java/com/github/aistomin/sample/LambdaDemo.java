17d7484adbe8acc12b4e683ee277d32d12b8f770
aistomin <andrej.istomin.ikeen@gmail.com>
2018-01-26 14:48:11 +0100

    LambdaDemo.java: the method with searches for max element in array was added.

--- src/main/java/com/github/aistomin/sample/LambdaDemo.java
+++ src/main/java/com/github/aistomin/sample/LambdaDemo.java
@@ -1,22 +1,29 @@
 package com.github.aistomin.sample;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by aistomin on 25.01.18.
- *
+ * <p>
  * The class which contains demo examples of Java8 lambda expressions.
  */
 final public class LambdaDemo {
 
     /**
-     * Sample method.
-     *
-     * @return Always returns 666.
+     * Find max element in list.
+     * @param list The list.
+     * @return Max element or null if the list is empty.
      */
-    int sample() {
-        return 666;
+    Integer max(final List<Integer> list) {
+        return list.stream().max(Integer::compareTo).orElse(null);
     }
 
     public static void main(String[] args) {
-        System.out.println("Output: " + new LambdaDemo().sample());
+        final List<Integer> list = new ArrayList<>(3);
+        list.add(3);
+        list.add(0);
+        list.add(3);
+        System.out.println("Output: " + new LambdaDemo().max(list));
     }
 }
--- src/test/java/com/github/aistomin/sample/LambdaDemoTest.java
+++ src/test/java/com/github/aistomin/sample/LambdaDemoTest.java
@@ -1,5 +1,7 @@
 package com.github.aistomin.sample;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -11,10 +13,16 @@ import org.junit.Test;
 public class LambdaDemoTest {
 
     /**
-     * Check that .sample() method is working.
+     * Check that we correctly calculate find max element in list.
      */
     @Test
-    public void testSample() {
-        Assert.assertEquals(666, new LambdaDemo().sample());
+    public void testMax() {
+        final List<Integer> list = new ArrayList<>();
+        final LambdaDemo demo = new LambdaDemo();
+        Assert.assertNull(demo.max(list));
+        list.add(0);
+        Assert.assertEquals(new Integer(0), demo.max(list));
+        list.add(1);
+        Assert.assertEquals(new Integer(1), demo.max(list));
     }
 }
